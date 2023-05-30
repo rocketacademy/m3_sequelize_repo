71eee7b6fb2c00798ba29ff7b0d3b65e3f9747ba
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-05-30 16:18:15 +0800

    aligning video content

--- db/seeders/20221130025626-1_primary_tables.js
+++ db/seeders/20221130025626-1_primary_tables.js
@@ -16,30 +16,24 @@ module.exports = {
     await queryInterface.bulkInsert("fruits", [
       {
         name: "Apple",
-        description: "This apple is crisp and sweet",
+        taste: "Sweet",
         colour: "Red",
         stock: 140,
         price: 15,
-        image_url:
-          "https://images.unsplash.com/photo-1570913149827-d2ac84ab3f9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
       },
       {
         name: "Kiwi",
-        description: "Sweet",
+        taste: "Sweet",
         colour: "The kiwis have a nice balaence of sweet and sour",
         stock: 120,
         price: 20,
-        image_url:
-          "https://images.unsplash.com/photo-1585059895524-72359e06133a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
       },
       {
         name: "Banana",
-        description: "Unbruised Banana good for potassium",
+        taste: "Sweet",
         colour: "Yellow",
         stock: 140,
         price: 10,
-        image_url:
-          "https://images.unsplash.com/photo-1528825871115-3581a5387919?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80",
       },
     ]);
 
