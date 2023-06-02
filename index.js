d414cfa216e6f564146febddff01142eecb39db4
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-06-02 10:30:39 +0800

    adding cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const db = require("./db/models/index");
 const { fruit } = db;
@@ -14,6 +15,7 @@ const fruitRouter = new FruitRouter(fruitController, express);
 // Setting up middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use("/fruit", fruitRouter.route());
 
